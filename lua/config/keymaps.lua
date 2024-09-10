d0e98f5a84e207ed1de31210d33227ceab1b3c7e
JoaoVictorGI <joaovictor.gizidoro@pm.me>
2024-09-10 17:49:27 -0300

    first commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,21 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- local discipline = require("custom/discipline")
+-- discipline.cowboy()
+
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- New tab
+keymap.set("n", "te", ":tabedit", opts)
+keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
+
+-- Diagnostic keymaps
+keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
+keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,10 @@
+return {
+  -- add one dark/light
+  { "navarasu/onedark.nvim" },
+
+  -- add kanagawa
+  -- { "rebelot/kanagawa.nvim" },
+
+  -- add nightfox
+  -- { "EdenEast/nightfox.nvim" },
+}
